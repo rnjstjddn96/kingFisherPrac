9624f704f51991f2c541407bf5b521299a6b617d
rnjstjddn96 <rnjstjddn1996@gmail.com>
2020-07-01 15:20:43 +0900

    kingfisher와 collectionView를 이용한 이미지뷰

--- kingFisherPrac/ViewController.swift
+++ kingFisherPrac/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import Kingfisher
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return Images.imageUrls.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        var view = collectionView.dequeueReusableCell(withReuseIdentifier: "RowCell", for: indexPath) as! CustomCell
+        
+        view.imageview.kf.setImage(with: URL(string: Images.imageUrls[indexPath.row]))
+        return view
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width/3, height: collectionView.frame.width/3)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+}
 
+class CustomCell : UICollectionViewCell{
+    @IBOutlet weak var imageview: UIImageView!
+    
 }
 
